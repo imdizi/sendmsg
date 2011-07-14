c0c2b4c866f28f37387d0f7cae9009fd189e32d5
imdizi <jhkgus@naver.com>
2011-07-14 02:47:00 -0700

    send finish msg

---
+++ sendmsg.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'mechanize'
+
+agent = Mechanize.new
+agent.basic_auth 'jhkgus@naver.com', '111111'
+agent.post ‘'https://boxcar.io/notifications', ‘'notification[message]' => 'Finished'
