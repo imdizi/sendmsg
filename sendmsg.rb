c3886667d52b0977f16bb2fd87a40b74d79b09df
imdizi <jhkgus@naver.com>
2011-07-14 02:52:51 -0700

    fix error

--- sendmsg.rb
+++ sendmsg.rb
@@ -5,4 +5,4 @@ require 'mechanize'
 
 agent = Mechanize.new
 agent.basic_auth 'jhkgus@naver.com', '111111'
-agent.post ‘'https://boxcar.io/notifications', ‘'notification[message]' => 'Finished'
+agent.post 'https://boxcar.io/notifications','notification[message]' => 'Finished'
